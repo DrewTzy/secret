489751c86dfd64ea258e45d3aee008adf19a0fa1
JTzy <155743367+JTzy7@users.noreply.github.com>
2024-01-08 20:40:29 +0800

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -99,15 +99,6 @@ class Paper {
     });
   }
 }
-
-const refreshBtn = document.getElementById("btnRefresh");
-
-function handleClick() {
-  window.location.reload();
-}
-
-refreshBtn.addEventListener("click", handleClick);
-
 const papers = Array.from(document.querySelectorAll('.paper'));
 
 papers.forEach(paper => {
